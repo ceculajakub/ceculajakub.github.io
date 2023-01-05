91584a1c3617f6c2b305dc8526968a4b24719d73
jacu <jacu@ailleron.com>
2023-01-05 18:33:20 +0100

    TEST mockend

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
 const mainBtn = document.querySelector(".main-btn")
 
 mainBtn.addEventListener('click', function() {
-    mainBtn.style.backgroundColor = '#cdd3ff';
 })
 
 const API_URL = 'https://my.api.mockaroo.com/edi.json?key=953a0b00';
+const test_url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-fetch(API_URL)
-  .then(response => response.json())
-  .then(data => {
-    // const playerArray = document.getElementById('player-array');
-
-    // data.players.forEach(player => {
-    //   const div = document.createElement('div');
-    //   div.textContent = JSON.stringify(player);
-    //   playerArray.appendChild(div);
-    console.log(data);
-    });
+fetch(test_url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      // const dataArray = document.getElementById('player-array');
+      // data.forEach(datum => {
+      //   const div = document.createElement('div');
+      //   div.textContent = JSON.stringify(datum);
+      //   dataArray.appendChild(div);
+      });
